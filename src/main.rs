32abd2e689e36eb6984be9820c1e5e401bdfd73b
TheShmill <21013808+TheShmill@users.noreply.github.com>
2021-11-01 08:48:13 -0400

    Added dotenv, tracing, serenity, and tokio dependencies Added .env.example Did some basic setup in main Added .env to .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
+    dotenv::dotenv()?;
+    tracing_subscriber::fmt().init();
+    Ok(())
 }
