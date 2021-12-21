0de2597b0af76b3a5bf1b0770561c0fdaccf1a3b
NiteBlock <niteblock@gmail.com>
2021-12-21 14:44:03 +0100

    fix prefabs

--- src/commands/prefabs.rs
+++ src/commands/prefabs.rs
@@ -1,54 +1,112 @@
-use serenity::{
-    prelude::Context,
-    async_trait
-};
-use serde_json::{value::Value, from_reader};
 use crate::commands::Command;
-use std::sync::Arc;
+use base64::decode;
+use serde_json::value::Value;
+use serenity::{
+    async_trait,
+    model::{
+        interactions::{
+            application_command::{ApplicationCommandInteraction, ApplicationCommandOptionType},
+            InteractionResponseType,
+        },
+        prelude::InteractionApplicationCommandCallbackDataFlags,
+    },
+    prelude::Context,
+    utils::Color,
+};
+use std::collections::HashMap;
 use std::fs;
+use std::sync::Arc;
 
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Field {
-    name: String,
-    value: String
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Embed {
-    title: String,
-    description: String,
-    fields: Vec<Field>,
-    footer: String,
-    color: String
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Message {
-    embed: Option<Embed>,
-    content: Option<String>
-}
+type Message = HashMap<String, Value>;
+type Messages = HashMap<String, Vec<Message>>;
+type Prefabs = HashMap<String, Messages>;
 
 pub struct Prefab {
-    inner: Arc<Inner>
+    inner: Arc<Inner>,
 }
 
 struct Inner {
+    prefabs: Prefabs,
 }
 
 #[async_trait]
 impl Command for Prefab {
+    fn name(&self) -> String {
+        "prefab".to_string()
+    }
+    async fn register(&self, ctx: &Context) -> crate::Result<()> {
+        crate::GUILD
+            .create_application_command(&ctx, |c| {
+                c.name(self.name())
+                    .description("Sends a given prefab.")
+                    .create_option(|o| {
+                        o.name("name")
+                            .description("Select the prefab that you would like to send.")
+                            .required(true)
+                            .kind(ApplicationCommandOptionType::String);
+                        for (name, _) in &self.inner.prefabs {
+                            o.add_string_choice(name, name);
+                        }
+                        o
+                    })
+            })
+            .await?;
+        Ok(())
+    }
+    async fn run(
+        &self,
+        ctx: &Context,
+        command: &ApplicationCommandInteraction,
+    ) -> crate::Result<()> {
+        command
+            .create_interaction_response(&ctx, |r| {
+                r.interaction_response_data(|d| {
+                    d.flags(InteractionApplicationCommandCallbackDataFlags::EPHEMERAL)
+                })
+                .kind(InteractionResponseType::DeferredChannelMessageWithSource)
+            })
+            .await?;
+        let s = command.data.options[0].value.clone().unwrap().to_string();
+        let mut i = s.chars();
+        i.next();
+        i.next_back();
+        let i = i.as_str();
+        let m: Messages = self.inner.prefabs[i].clone();
+
+        for message in &m["messages"] {
+            let _ = &ctx
+                .http
+                .send_message(command.channel_id.0, &message["data"])
+                .await;
+        }
+        command
+            .edit_original_interaction_response(&ctx, |r| {
+                r.create_embed(|e| {
+                    e.title("Prefab Sent")
+                        .description(format!("The prefab `{}` has been sent.", i))
+                        .color(Color::MAGENTA)
+                })
+            })
+            .await?;
+
+        Ok(())
+    }
     fn new() -> Box<Self>
     where
         Self: Sized,
     {
-        let data_str = fs::read_to_string("prefabs.json");
-        println!("{:?}", data_str);
+        let data_str = fs::read_to_string("prefabs.json").unwrap();
+        let data = serde_json::from_str::<HashMap<String, String>>(&data_str).unwrap();
+
+        let mut prefabs: Prefabs = HashMap::new();
+        for (prefab_name, prefab_value) in data {
+            let s = decode(prefab_value).unwrap();
+            let s = String::from_utf8(s).unwrap();
+            let d = serde_json::from_str::<Messages>(&s).unwrap();
+            prefabs.insert(prefab_name, d);
+        }
         Box::new(Prefab {
-            inner: Arc::new(Inner {
-                
-            }),
+            inner: Arc::new(Inner { prefabs }),
         })
     }
 }
