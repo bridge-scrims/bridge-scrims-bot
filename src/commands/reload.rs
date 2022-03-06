bab19cda786409f14e7aa6ab8d1f02e11573d273
Tom van Dijk <18gatenmaker6@gmail.com>
2022-03-06 15:17:00 +0100

    fixup! feat(Reload): Reload the slash commands with /reload

--- src/commands/reload.rs
+++ src/commands/reload.rs
@@ -1,6 +1,9 @@
 use serenity::{
-    async_trait, client::Context,
-    model::interactions::application_command::{ApplicationCommandInteraction, ApplicationCommandPermissionType},
+    async_trait,
+    client::Context,
+    model::interactions::application_command::{
+        ApplicationCommandInteraction, ApplicationCommandPermissionType,
+    },
 };
 
 use crate::consts::CONFIG;
@@ -13,26 +16,41 @@ impl super::Command for Reload {
         String::from("reload")
     }
     async fn register(&self, ctx: &Context) -> crate::Result<()> {
-        let cmd = CONFIG.guild.create_application_command(&ctx.http, |cmd| {
-            cmd.name(self.name())
-                .description("Reloads application commands.")
-                .default_permission(false)
-        }).await?;
-
-        CONFIG.guild.create_application_command_permission(&ctx.http, cmd.id, |perm| {
-            perm.create_permission(|perm| {
-                perm.kind(ApplicationCommandPermissionType::Role)
-                    .id(CONFIG.staff.0)
+        let cmd = CONFIG
+            .guild
+            .create_application_command(&ctx.http, |cmd| {
+                cmd.name(self.name())
+                    .description("Reloads application commands.")
+                    .default_permission(false)
             })
-        }).await?;
+            .await?;
+
+        CONFIG
+            .guild
+            .create_application_command_permission(&ctx.http, cmd.id, |perm| {
+                perm.create_permission(|perm| {
+                    perm.kind(ApplicationCommandPermissionType::Role)
+                        .id(CONFIG.staff.0)
+                })
+            })
+            .await?;
 
         Ok(())
     }
     async fn run(
         &self,
         ctx: &Context,
-        command: &ApplicationCommandInteraction,
+        _command: &ApplicationCommandInteraction,
     ) -> crate::Result<()> {
+        let commands = CONFIG
+            .guild
+            .get_application_commands(&ctx.http)
+            .await?
+            .into_iter()
+            .map(|x| x.id);
+        for id in commands {
+            CONFIG.guild.delete_application_command(&ctx.http, id).await?;
+        }
         Ok(())
     }
     fn new() -> Box<Self> {
