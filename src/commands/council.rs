dc4dfdf4c042adf9fd123148a660569f4037635a
Nite <Niteblock@gmail.com>
2022-04-07 12:48:39 +0200

    test

--- src/commands/council.rs
+++ src/commands/council.rs
@@ -83,6 +83,7 @@ impl Command for Council {
         } else {
             let v = self.councils.data().await;
             tracing::error!("Council not found {}, {:?}", name, v);
+            tracing::info!("{:?}", v.get("Prime"));
             command
                 .edit_original_interaction_response(&ctx, |r| {
                     r.create_embed(|e| {
