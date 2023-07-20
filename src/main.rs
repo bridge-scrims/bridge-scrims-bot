fe5cc4e7103c1183c5f36e5e89189bf958577c27
WhatCats <whatcater@gmail.com>
2023-07-20 18:26:59 +0200

    clean up entry point
    - docker automatically saves logs, so no need to write to files
    - application id does not need to be fetched and set
    - ctrl + c listener does not work on Linux production server

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use serenity::http::Http;
 use serenity::model::gateway::GatewayIntents;
 use serenity::Client;
-use tracing_subscriber::{
-    filter::LevelFilter, fmt::Layer, layer::SubscriberExt, Layer as _, Registry,
-};
+use tracing_subscriber::filter::LevelFilter;
 
 use crate::consts::{CONFIG, SECRETS};
 use crate::handler::Handler;
@@ -19,28 +16,12 @@ mod macros;
 mod model;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let file_appender = tracing_appender::rolling::daily("logs", "rolling.log");
-    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-    let filter = LevelFilter::INFO;
-    let subscriber = Registry::default()
-        .with(Layer::default().pretty().with_filter(filter))
-        .with(
-            Layer::default()
-                .pretty()
-                .with_ansi(false)
-                .compact()
-                .with_writer(non_blocking)
-                .with_filter(filter),
-        );
+async fn main() {
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_max_level(LevelFilter::INFO)
+        .init();
 
-    tracing::subscriber::set_global_default(subscriber).unwrap();
-
-    let application_id = Http::new(&SECRETS.bot_token)
-        .get_current_application_info()
-        .await?
-        .id
-        .0;
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT
         | GatewayIntents::GUILD_MEMBERS
@@ -49,20 +30,11 @@ async fn main() -> Result<()> {
         | GatewayIntents::GUILD_PRESENCES;
 
     let mut client = Client::builder(&SECRETS.bot_token, intents)
-        .application_id(application_id)
         .event_handler(Handler::new())
-        .await?;
-    let shard_manager = client.shard_manager.clone();
-    tokio::spawn(async move {
-        tokio::signal::ctrl_c()
-            .await
-            .expect("Could not listen for ctrl+c");
-        tracing::info!("Shutting down");
-        tracing::info!("Ending process...");
-        shard_manager.lock().await.shutdown_all().await;
-    });
+        .await
+        .expect("Failed to connect to Discord!");
+
     while let Err(err) = client.start().await {
         tracing::error!("{}", err);
     }
-    Ok(())
 }
