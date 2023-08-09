64b9e5dabc97a0a5b62167f78fb9724fd7dfb189
TheShmill <21013808+TheShmill@users.noreply.github.com>
2023-08-09 10:00:56 -0400

    Database schema uses timestamps w/ timezone

--- migrations/20230718021920_bridge-scrims.sql
+++ migrations/20230718021920_bridge-scrims.sql
@@ -3,32 +3,32 @@
 
 CREATE TABLE scheduled_scrim_unban (
                 user_id bigint PRIMARY KEY,
-                expires_at timestamp,
+                expires_at TIMESTAMP WITH TIME ZONE,
                 roles bigint[] NOT NULL
             );
 CREATE TABLE user_note (
-                user_id bigint,
-                id integer,
-                created_at timestamp NOT NULL,
-                note text NOT NULL,
-                creator bigint NOT NULL,
+                user_id BIGINT,
+                id INTEGER,
+                created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+                note TEXT NOT NULL,
+                creator BIGINT NOT NULL,
                 PRIMARY KEY(user_id, id)
             );
 CREATE TABLE reaction (
-                user_id bigint PRIMARY KEY,
-                emoji text NOT NULL,
-                trigger text NOT NULL
+                user_id BIGINT PRIMARY KEY,
+                emoji TEXT NOT NULL,
+                trigger TEXT NOT NULL
             );
 CREATE TABLE screenshare (
-                channel_id bigint PRIMARY KEY,
-                creator_id bigint NOT NULL,
-                in_question bigint NOT NULL
+                channel_id BIGINT PRIMARY KEY,
+                creator_id BIGINT NOT NULL,
+                in_question BIGINT NOT NULL
             );
 CREATE TABLE screensharer_stats (
-                user_id bigint PRIMARY KEY,
-                freezes integer NOT NULL
+                user_id BIGINT PRIMARY KEY,
+                freezes INTEGER NOT NULL
             );
 CREATE TABLE freezes (
-                user_id bigint PRIMARY KEY,
-                roles bigint[] NOT NULL
+                user_id BIGINT PRIMARY KEY,
+                roles BIGINT[] NOT NULL
             );
